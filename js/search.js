30eebbd6230254fa702eb057a7d75b5653f6ab47
Jonathan Bardo <bardo.jonathan@gmail.com>
2015-01-22 14:37:52 -0500

    Module pattern

--- js/search.js
+++ js/search.js
@@ -1,90 +1,48 @@
 /** @namespace */
-var SearchClass =  {
+var JB = JB || {};
 
-  //Hold the previous html without highlights
-  oldHTML: '',
+JB.SearchText = (function (){
+  var params = {},
+      oldHTML = '';
 
-  /**
-   * Class constructor
-   * @param {searchArgs} obj All search parameters in order for this class to work
-   * @returns {void}
-   */
-  init: function( searchArgs ) {
-    if ( !this._isObject(searchArgs) ) {
-      this._log('Wrong parameters type');
-      return;
-    }
 
-    //Required args
-    var required = [
-      'txtContainer',
-      'searchInput',
-      'searchSubmit',
-      'searchResult',
-      'resultFoundTxt',
-      'highlightColor'
-    ];
-
-    //Assign passed parameters to class properties
-    for( var i=0; i<required.length; i++ ) {
-      if( searchArgs.hasOwnProperty( required[i] ) ) {
-        this[required[i]] = searchArgs[ required[i] ];
-      }else {
-        this._log('Class parameter missing');
-        return;
-      }
-    }
-
-    //Store textContainer HTML so it can be restore 
-    this.oldHTML = this.txtContainer.innerHTML;
-
-    //Event binders
-    this.searchSubmit.addEventListener( 'click', this.triggerSearch );
-  },
-
-  /**
-   * Get user input string and search for occurences
-   * @param {string} str The search string
-   * @returns {int}
-   */
-  triggerSearch: function(event) {
-    var _this = SearchClass;
+  _triggerSearch = function (event) {
     event.preventDefault();
 
     //Get number of results
     var results = [
-      _this._searchResults(_this.searchInput.value),
-      _this.searchInput.value
+      _searchResults(params.searchInput.value),
+      params.searchInput.value
     ]
 
     //Print the number of results
-    _this.searchResult.textContent = _this._sprintf( _this.resultFoundTxt, results );
+    params.searchResult.textContent = _sprintf(params.resultFoundTxt, results);
 
     return false;
-  },
+  }
 
   /**
    * Get the number of search results
    * @param {string} str The search string
    * @returns {int}
    */
-  _searchResults: function( str ) {
+  _searchResults = function (str) {
     //Get search RegExp
-    var regExp = new RegExp( this._escapeRegExp( str.toLowerCase() ), 'ig' );
+    var regExp = new RegExp( _escapeRegExp( str.toLowerCase() ), 'ig' );
 
     //Get container innerText
-    var innerText = ( this.txtContainer.textContent || this.txtContainer.innerText );
+    var innerText = (params.txtContainer.textContent || params.txtContainer.innerText);
 
     //Try to match result
     var results = innerText.match(regExp);
 
     //Restore old HTML before modification
-    this.txtContainer.innerHTML = this.oldHTML;
+    params.txtContainer.innerHTML = oldHTML;
 
     //Get all occurances selection and highlight
-    while ( match = regExp.exec( innerText ) ) {
-      this._setSelectionRange( this.txtContainer, match.index, regExp.lastIndex );
-      this._highligthText( this.highlightColor );
+    while (match = regExp.exec(innerText)) {
+      _setSelectionRange( params.txtContainer, match.index, regExp.lastIndex );
+      _highligthText( params.highlightColor );
     }
 
     //Remove selection
@@ -92,7 +50,7 @@ var SearchClass =  {
 
     //Return number of results
     return (results !== null) ? results.length : 0;
-  },
+  }
 
   /**Highlight functions**/
 
@@ -101,31 +59,32 @@ var SearchClass =  {
    * @param {obj} node The node element
    * @returns {array}
    */
-  _setSelectionRange: function ( node, start, end ) {
-    var rg = document.createRange();
-    rg.selectNodeContents( node );
+  _setSelectionRange = function (node, start, end) {
+    var rg = document.createRange(),
+        txtNode,
+        txtNodes = _getTxtNodesIn( node ),
+        startFound = false,
+        charPos = 0,
+        endCharPos,
+        sel,
+        i = -1;
 
-    var txtNode;
-    var txtNodes = this._getTxtNodesIn( node );
-    var startFound = false;
-    var charPos = 0;
-    var endCharPos;
+    rg.selectNodeContents(node);
 
-    var i = -1; 
-    while ( true ) {
+    while (true) {
         txtNode = txtNodes[ ++i ];
         endCharPos = charPos + txtNode.length;
 
         if (
           ! startFound 
           && start >= charPos 
-          && ( start < endCharPos || ( start === endCharPos && i < txtNodes.length ) ) 
+          && (start < endCharPos || (start === endCharPos && i < txtNodes.length)) 
         ) {
-            rg.setStart( txtNode, start - charPos );
+            rg.setStart(txtNode, start - charPos);
             startFound = true;
         }
 
-        if ( startFound && end <= endCharPos ) {
+        if (startFound && end <= endCharPos) {
             rg.setEnd( txtNode, end - charPos );
             break;
         }
@@ -133,36 +92,36 @@ var SearchClass =  {
         charPos = endCharPos;
     }
 
-    var sel = window.getSelection();
+    sel = window.getSelection();
     sel.removeAllRanges();
     sel.addRange(rg);
-  },
+  }
 
   /**
    * Function to highlight text based on 
    * @param {object} node The given node
    * @returns {array}
    */
-  _highligthText: function ( color ) {
+  _highligthText = function (color) {
       sel = window.getSelection();
-      if ( sel.rangeCount && sel.getRangeAt ) {
+      if (sel.rangeCount && sel.getRangeAt) {
           var rg = sel.getRangeAt(0);
       }
 
       document.designMode = "on";
 
-      if ( rg ) {
+      if (rg) {
           sel.removeAllRanges();
           sel.addRange( rg );
       }
 
       // Moz and Webkit/Blink functions
-      if ( ! document.execCommand("HiliteColor", false, color) ) {
+      if (!document.execCommand("HiliteColor", false, color)) {
           document.execCommand("BackColor", false, color);
       }
 
       document.designMode = "off";
-  },
+  }
 
 
   /**Class helpers**/
@@ -172,76 +131,110 @@ var SearchClass =  {
    * @param {object} node The given node
    * @returns {array}
    */
-  _getTxtNodesIn: function ( node ) {
+  _getTxtNodesIn = function (node) {
     var txtNodes = [];
 
     //If we have a text node https://developer.mozilla.org/en-US/docs/Web/API/Node.nodeType
-    if ( node.nodeType === 3 ) {
+    if (node.nodeType === 3) {
         txtNodes.push(node);
     } else {
         var children = node.childNodes;
         var length = children.length;
         
         //Recursive function to get all text nodes
-        for ( var i = 0; i < length; i++ ) {
-            txtNodes.push.apply( txtNodes, this._getTxtNodesIn( children[ i ] ) );
+        for (var i = 0; i < length; i++) {
+            txtNodes.push.apply(txtNodes, _getTxtNodesIn(children[i]));
         }
     }
 
     return txtNodes;
-  },
+  }
 
   /**
    * String escape for use in RegExp expression
    * @param {string} str The RegExp string
    * @returns {string} The escape RegExp string
    */
-  _escapeRegExp: function( str ) {
+  _escapeRegExp = function (str) {
     return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-  },
+  }
 
   /**
    * Kind of mimic php sprintf function
    * @param {string} str The search string
    * @returns {string} args The array of arguments
    */
-  _sprintf: function( str, args ) {
-    for( var i=0; i < args.length; i++ ) {
-      str = str.replace( /%s/, args[i] );
+  _sprintf = function (str, args) {
+    for (var i=0; i < args.length; i++) {
+      str = str.replace(/%s/, args[i]);
     }
     return str;
-  },
+  }
 
    /**
    * Check if the given argument is an object
    * @param {string} obj The object
    * @returns {bool} 
    */
-  _isObject: function( obj ) {
+  _isObject = function (obj) {
     return Object.prototype.toString.call(obj) === '[object Object]';
-  },
+  }
 
   /**
    * JavaScript Helper log helper
    * @param {string} msg The log message
    * @returns {void} 
    */
-  _log: function( msg ) {
-    if( window.console ) {
+  _log = function (msg) {
+    if (window.console) {
       console.log(msg);
     }
   }
 
-}
+  return {
+    init: function (searchArgs) {
+      if (!_isObject(searchArgs)) {
+        _log('Wrong parameters type');
+        return;
+      }
+
+      //Required args
+      var required = [
+        'txtContainer',
+        'searchInput',
+        'searchSubmit',
+        'searchResult',
+        'resultFoundTxt',
+        'highlightColor'
+      ];
+
+      //Assign passed parameters to class properties
+      for (var i=0; i<required.length; i++) {
+        if (searchArgs.hasOwnProperty(required[i])) {
+          params[required[i]] = searchArgs[required[i]];
+        } else {
+          _log('Class parameter missing');
+          return;
+        }
+      }
+
+      //Store textContainer HTML so it can be restore 
+      oldHTML = params.txtContainer.innerHTML;
+
+      //Event binders
+      params.searchSubmit.addEventListener('click', _triggerSearch);
+    }
+  }
+})();
 
 //Class parameters
 var searchArgs =  {
-  txtContainer:   document.getElementById('search_text'),
-  searchInput:    document.getElementById('search_input'),
-  searchSubmit:   document.getElementById('search_submit'),
-  searchResult:   document.getElementById('search_result'),
+  txtContainer: document.getElementById('search_text'),
+  searchInput: document.getElementById('search_input'),
+  searchSubmit: document.getElementById('search_submit'),
+  searchResult: document.getElementById('search_result'),
   resultFoundTxt: 'Found %s occurences of the word \"%s\" in the below text.',
   highlightColor: 'yellow'
 }
 
-SearchClass.init(searchArgs);
+JB.SearchText.init(searchArgs);
